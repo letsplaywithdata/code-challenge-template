6ba059d01a3a85fe8dacc642e9c201644ea3c62d
Yogesh Nizzer <50980928+letsplaywithdata@users.noreply.github.com>
2023-02-21 09:29:24 -0500

    Update ingest_data.py

--- src/ingestion/ingest_data.py
+++ src/ingestion/ingest_data.py
@@ -1,17 +1,21 @@
 import pandas as pd
+from sqlalchemy import insert
 from sqlalchemy.orm import Session
 from tqdm import tqdm
 
-def upload_data(class_Name, data: pd.DataFrame, session: Session):
+def upload_data(class_Name, data: pd.DataFrame, session: Session, batch_size=1000):
     """
     Uploads data to the database using bulk insert for faster performance.
     """
     # Convert dataframe to list of dictionaries
     data_dict = data.to_dict('records')
 
-    # Bulk insert data into database table
+    # Insert data into database table in chunks
     num_records = len(data_dict)
     with tqdm(total=num_records, desc=f"Uploading {class_Name.__tablename__}") as progress:
-        session.bulk_insert_mappings(class_Name, data_dict)
-        session.commit()
-        progress.update(num_records)
+        for i in range(0, num_records, batch_size):
+            chunk = data_dict[i:i+batch_size]
+            stmt = insert(class_Name).values(chunk)
+            session.execute(stmt)
+            session.commit()
+            progress.update(len(chunk))
